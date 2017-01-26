fce81dee9dfc8a5cd7c7c8bf825a263a61920493
Deniel Horvatic <deniel.horvatic@porscheidigitallab.com>
2017-01-26 18:50:05 +0100

    Added all weeks

--- src/scraper_heise.py
+++ src/scraper_heise.py
@@ -12,16 +12,18 @@ class ArticlesSpider(scrapy.Spider):
     #  archiv_woche > ul:nth-child(3) > li:nth-child(1) > a
 
     def parse(self, response):  # archiv_woche > ul:nth-child(3) > li:nth-child(2) > a
+        x = 0
         for daysPerWeek in response.css('#archiv_woche'):
             for articlesPerDay in daysPerWeek.css('ul '):
                 for article in articlesPerDay.css('li'):
                     yield {
-                        'creationDate': daysPerWeek.css('h4::text').extract()[2],
+                        'creationDate': daysPerWeek.css('h4::text').extract()[x],
                         'link': article.css('a::attr(href)').extract_first(),
                         'article': article.css('a::text').extract_first(),
                     }
+                x += 1
+        next_page = response.css('#archiv_woche > p > a.archiv_woche_navigation_next::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
-                    # next_page = response.css('li.next a::attr("href")').extract_first()
-                    # if next_page is not None:
-                    #     next_page = response.urljoin(next_page)
-                    #     yield scrapy.Request(next_page, callback=self.parse)
