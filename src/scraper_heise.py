385cde4de98cc8182ad54ec5b5c51efc8ab4ecd9
Deniel Horvatic <deniel.horvatic@porscheidigitallab.com>
2017-01-26 19:33:16 +0100

    Change from json to csv file

--- src/scraper_heise.py
+++ src/scraper_heise.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.utils.project import get_project_settings
 
 
 class ArticlesSpider(scrapy.Spider):
@@ -11,7 +12,12 @@ class ArticlesSpider(scrapy.Spider):
     #  archiv_woche > ul:nth-child(3) > li:nth-child(1) > a
     #  archiv_woche > ul:nth-child(3) > li:nth-child(1) > a
 
-    def parse(self, response):  # archiv_woche > ul:nth-child(3) > li:nth-child(2) > a
+    # set up settings
+    settings = get_project_settings()
+    settings.overrides['ITEM_PIPELINES'] = {'__main__.MyPipeline': 1}
+
+
+    def parse(self, response):
         x = 0
         for daysPerWeek in response.css('#archiv_woche'):
             for articlesPerDay in daysPerWeek.css('ul '):
@@ -22,8 +28,8 @@ class ArticlesSpider(scrapy.Spider):
                         'article': article.css('a::text').extract_first(),
                     }
                 x += 1
-        next_page = response.css('#archiv_woche > p > a.archiv_woche_navigation_next::attr(href)').extract_first()
+        next_page = response.css(
+            '#archiv_woche > p > a.archiv_woche_navigation_next::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
-
--- start-script.sh
+++ start-script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Start scrapping
 echo "Start - scrapping"
-scrapy runspider src/scraper_heise.py -o resources/data/articles/heise_articles.json
+scrapy runspider src/scraper_heise.py -t csv -o resources/data/articles/heise_articles.csv
 echo "End   - scrapping"
